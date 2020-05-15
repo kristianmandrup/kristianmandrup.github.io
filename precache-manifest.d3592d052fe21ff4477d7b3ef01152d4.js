9b7bf1538939d8bb81a46cf6a6004cd798bb677e
Kristian Mandrup <kmandrup@gmail.com>
2020-05-15 13:27:01 +0100

    Updates

---
+++ precache-manifest.d3592d052fe21ff4477d7b3ef01152d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed155c43e227d947687d586742d882c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b988f35532a887218076",
+    "url": "/static/css/main.ad511f6b.chunk.css"
+  },
+  {
+    "revision": "7a3cb0ed1d2cd42729d3",
+    "url": "/static/js/2.002bdf40.chunk.js"
+  },
+  {
+    "revision": "b6a57e0678f5f23c667a8321b1b88197",
+    "url": "/static/js/2.002bdf40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b988f35532a887218076",
+    "url": "/static/js/main.be81d3da.chunk.js"
+  },
+  {
+    "revision": "6061417fe8fe4517e4b9",
+    "url": "/static/js/runtime-main.e07b4bc6.js"
+  }
+]);
